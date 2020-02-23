d72eb1a91cc851acd18e63adbf87e007741fea3d
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-02-23 07:02:17 -0300

    fix: syntax

--- source/components/button.js
+++ source/components/button.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import styled from 'styled-components/native';
 
+const ButtonView = styled.TouchableHighlight`
+  alignItems: center;
+  background-color: ${props => props.bg};
+  height: 50;
+  justifyContent: center;
+  padding: 0 20px;
+  width: 50%;
+`;
+
+const TextView = styled.Text`
+  color: #fff;
+  fontSize: 20;
+  textShadow: 0 0 3px rgba(0,0,0,0.6);
+`;
+
 class Button extends Component {
   static defaultProps = {
     bg: '#ff00ff',
@@ -13,25 +28,10 @@ class Button extends Component {
 
     return (
       <ButtonView underlayColor={underlayColor} onPress={onPress} bg={bg}>
-        <Text>{children}</Text>
+        <TextView>{children}</TextView>
       </ButtonView>
     );
   }
 }
-
-const ButtonView = styled.TouchableHighlight`
-  alignItems: center;
-  background-color: ${props => props.bg};
-  height: 50;
-  justifyContent: center;
-  padding: 0 20px;
-  width: 50%;
-`;
-
-const Text = styled.Text`
-  color: #fff;
-  fontSize: 20;
-  textShadow: 0 0 3px rgba(0,0,0,0.6);
-`;
 
 export default Button;
